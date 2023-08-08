bb44c24cfb7a41e84c078d8cae8b916db73dc095
Corbie <nicholaicorbie1@gmail.com>
2023-08-08 04:00:16 +0100

    adding finished coffee machine code

--- Personal Projects/100 Days of Code/CoffeeMachine/CoffeeMachine.py
+++ Personal Projects/100 Days of Code/CoffeeMachine/CoffeeMachine.py
@@ -1,151 +1,183 @@
 class MenuItems:
+
+    def __init__(self):
+
+        self.expresso = [50, 0, 18, 1.50]
+
+        self.latte = [200, 150, 24, 2.50]
+
+        self.cappuccino = [250, 100, 24, 3.00]
+        pass
         
-    expresso = [50, 0, 18, 1.50]
-
-    latte = [200, 150, 24, 2.50]
-
-    cappuccino = [250, 100, 24, 3.00]
+    
 
 class MachineItems:
 
-    machine_water = 300
+    def __init__(self):
 
-    machine_milk = 200
+        self.machine_water = 300
 
-    machine_coffee = 100
+        self.machine_milk = 200
 
-    machine_money = 0
+        self.machine_coffee = 100
 
-    coffee_cost = None
+        self.machine_money = 0
 
-    customer_money = 0
+        self.coffee_cost = 0
+
+        self.customer_money = 0
+        pass
+    
 
 class Money:
 
-    penny = 0.01
+    def __init__(self):
+        
+        self.penny = 0.01
 
-    nickel = 0.05
+        self.nickel = 0.05
 
-    dime = 0.10
-    
-    quarter = 0.25
+        self.dime = 0.10
+        
+        self.quarter = 0.25
+        pass
+
 
 class MachineFunctions:
 
-    def choose_coffee(self, customer_money):
-        try:
-            choice = (input("What would you like? (expresso/latte/cappuccino): ")).lower()
-            if choice == "expresso":
-                coffee_cost = MenuItems.expresso[3]    
-                while customer_money < coffee_cost:
-                    print(f"Sorry, ${customer_money} is insufficient money to make your {choice}.")
-                    customer_money = MachineFunctions.process_cust_money                  
-            elif choice == "latte":
-                coffee_cost = MenuItems.latte[3]
-                while customer_money < coffee_cost:    
-                    print(f"Sorry, ${customer_money} is insufficient money to make your {choice}.")
-                    customer_money = MachineFunctions.process_cust_money 
-            elif choice == "cappuccino":
-                coffee_cost = MenuItems.latte[3]
-                while customer_money < coffee_cost:     
-                    print(f"Sorry, ${customer_money} is insufficient money to make your {choice}.")
-                    customer_money = MachineFunctions.process_cust_money 
-            else:
-                print(f"This input is invalid. Please choose expresso/latte/cappuccino.")
-        except TypeError:
-            print(f"{choice} is an invalid input. Please choose expresso/latte/cappuccino.")
+    def choose_coffee(self, choice_check):
+        while choice_check == True:
+            try:
+                choice = (input("What would you like? (expresso/latte/cappuccino/cancel): ")).lower()
+                if choice == "expresso":
+                    choice_check = False                  
+                elif choice == "latte":
+                    choice_check = False
+                elif choice == "cappuccino":
+                    choice_check = False
+                elif choice == "cancel":
+                    choice_check = False
+                    return choice
+                elif choice == "report":
+                    choice_check = False
+                    return choice
+                else:
+                    print(f"This input is invalid. Please choose expresso/latte/cappuccino.")
+            except TypeError:
+                print(f"{choice} is an invalid input. Please choose expresso/latte/cappuccino.")
         return choice
     
-    def process_cust_money(self, customer_money, quarter=Money.quarter, dime=Money.dime, nickel=Money.nickel, penny=Money.penny):
-        try:
-            quarters = int(input("Quarters: "))
-            dimes = int(input("Dimes: "))
-            nickels = int(input("Nickels: "))
-            pennies = int(input("Pennies: "))
-            customer_money += (quarters * quarter) + (dimes * dime) + (nickels * nickel) + (pennies * penny)
-        except ValueError:
-                        print("This is an invalid input. Please input an integer amount of coins.")
+    def get_coffee_cost(self, choice):
+        if choice == "expresso":
+            coffee_cost = MenuItems().expresso[3]    
+        elif choice == "latte":
+            coffee_cost = MenuItems().latte[3]
+        elif choice == "cappuccino":
+            coffee_cost = MenuItems().cappuccino[3]
+        return coffee_cost
+    
+    def check_money(self, choice, customer_money):
+        if choice == "expresso":
+            coffee_cost = MenuItems().expresso[3]    
+            if customer_money < coffee_cost:
+                print(f"Sorry, ${round(customer_money, 2)} is insufficient money to make your {choice}.")
+            else:
+                return True
+        elif choice == "latte":
+            coffee_cost = MenuItems().latte[3]
+            if customer_money < coffee_cost:    
+                print(f"Sorry, ${round(customer_money, 2)} is insufficient money to make your {choice}.")
+            else:
+                return True
+        elif choice == "cappuccino":
+            coffee_cost = MenuItems().cappuccino[3]
+            if customer_money < coffee_cost:     
+                print(f"Sorry, ${round(customer_money, 2)} is insufficient money to make your {choice}.")
+            else:
+                return True
+        pass
+
+    def process_cust_money(self, machine_money, coffee_cost, customer_money, quarter, dime, nickel, penny, money_input_check=True):
+        if customer_money < coffee_cost:
+            while money_input_check == True:
+                try:
+                    quarters = int(input("Quarters: "))
+                    dimes = int(input("Dimes: "))
+                    nickels = int(input("Nickels: "))
+                    pennies = int(input("Pennies: "))
+                    add_money = (quarters * quarter) + (dimes * dime) + (nickels * nickel) + (pennies * penny)
+                    customer_money += add_money
+                    machine_money += add_money
+                    money_input_check = False
+                except ValueError:
+                                print("This is an invalid input. Please input an integer amount of coins.")
+        return [customer_money, machine_money]
+    
+    def take_cust_money(self, customer_money, coffee_cost):
+        customer_money -= coffee_cost
         return customer_money
 
-    def report(self, machine_water=MachineItems.machine_water, machine_milk=MachineItems.machine_milk, machine_coffee=MachineItems.machine_coffee):
-        print(f" water: {machine_water}ml\n milk: {machine_milk}ml\n coffee: {machine_coffee}g\n money: ${machine_money}")
+    def report(self, machine_water, machine_milk, machine_coffee, machine_money):
+        print(f" water: {machine_water}ml\n milk: {machine_milk}ml\n coffee: {machine_coffee}g\n money: ${round(machine_money, 2)}")
         pass
 
-    def balance(self, customer_money=MachineItems.customer_money):
-        print(f"Your current balance is ${customer_money}")
+    def balance(self, customer_money):
+        print(f"Your current balance is ${round(customer_money, 2)}")
         pass
     
-    def process_machine_money(self, action, machine_money=MachineItems.machine_money):
-        if action == "add":
-            machine_money += MachineFunctions.process_cust_money
-        elif action == "end":
-            machine_money -= MachineItems.customer_money
-        else:
-            return machine_money
+    def return_change(self, machine_money, customer_money):
+        machine_money -= customer_money
         return machine_money
 
-    def make_coffee(self, choice, machine_water=MachineItems.machine_water, machine_coffee=MachineItems.machine_coffee, machine_milk=MachineItems.machine_milk):
+    def make_coffee(self, choice, machine_water, machine_coffee, machine_milk):
         try:
             if choice == "expresso":
-                expresso = MenuItems.expresso
-                if machine_water < 50:
-                    print(f"Sorry,there is insufficient water in the machine to make your {choice}.")
-                elif machine_coffee < 18:
-                    print(f"Sorry,there is insufficient coffee in the machine to make your {choice}.")
+                expresso = MenuItems().expresso
+                if machine_water < 50 or machine_coffee < 18:
+                    print(f"Sorry,there are insufficient ingredients in the machine to make your {choice}.")
+                    return False
                 else:
                     machine_water -= expresso[0]
                     machine_milk -= expresso[1]
                     machine_coffee -= expresso[2]
+                    print(f"Here is your {choice}. Enjoy!")
+                    return [machine_water, machine_milk, machine_coffee]
             elif choice == "latte":
-                latte = MenuItems.latte
-                if machine_water < 200:
-                    print(f"Sorry,there is insufficient water in the machine to make your {choice}.")
-                elif machine_milk < 150:
-                    print(f"Sorry,there is insufficient milk in the machine to make your {choice}.")
-                elif machine_coffee < 18:
-                    print(f"Sorry,there is insufficient coffee in the machine to make your {choice}.")
+                latte = MenuItems().latte
+                if machine_water < 200 or machine_milk < 150 or machine_coffee < 18:
+                    print(f"Sorry,there are insufficient ingredients in the machine to make your {choice}.")
+                    return False
                 else:
                     machine_water -= latte[0]
                     machine_milk -= latte[1]
                     machine_coffee -= latte[2]
+                    print(f"Here is your {choice}. Enjoy!")
+                    return [machine_water, machine_milk, machine_coffee]
             elif choice == "cappuccino":
-                cappuccino = MenuItems.cappuccino
-                if machine_water < 250:
-                    print(f"Sorry,there is insufficient water in the machine to make your {choice}.")
-                elif machine_milk < 100:
-                    print(f"Sorry,there is insufficient milk in the machine to make your {choice}.")
-                elif machine_coffee < 24:
-                    print(f"Sorry,there is insufficient coffee in the machine to make your {choice}.")
+                cappuccino = MenuItems().cappuccino
+                if machine_water < 250 or machine_milk < 100 or machine_coffee < 24:
+                    print(f"Sorry,there are insufficient ingredients in the machine to make your {choice}.")
+                    return False
                 else:
                     machine_water -= cappuccino[0]
                     machine_milk -= cappuccino[1]
                     machine_coffee -= cappuccino[2]
+                    print(f"Here is your {choice}. Enjoy!") 
+                    return [machine_water, machine_milk, machine_coffee]         
             else:
-                print(f"This input is invalid. Please choose expresso/latte/cappuccino.")
+                print(f"This input is invalid. Please choose expresso/latte/cappuccino/cancel.")
+                return False
         except TypeError:
-            print(f"{choice} is an invalid input. Please choose expresso/latte/cappuccino.")
-        pass
+            print(f"{choice} is an invalid input. Please choose expresso/latte/cappuccino/cancel.")
+            return False
 
-    def restart():
-        while restart_check is True:
-            try:
-                restart = (input("Would you like another coffee? (Yes/No) ")).lower()
+    def end_greeting(self, customer_money):
+        if customer_money > 0:
+            print(f"Your change is ${round(customer_money, 2)}. Have a wonderful day!")
+        else:
+            print("Have a great day!")
 
-                if restart == "no":
-                    coffee_machine_on = False
-                    restart_check = False
-                    MachineFunctions.process_machine_money("end")
-                    print(f"Your change is ${customer_money}. Have a wonderful day!")
-                elif restart == "yes":
-                    restart_check = False
-                    customer_money = customer_money
-                    print(f"Your current credit balance is ${customer_money}.")
-                else:
-                    print(f"This input is invalid. Please choose Yes/No.")
-            except TypeError:
-                print(f"{restart} is an invalid input. Please choose Yes/No.")
-        pass
-
+   
 
 
 
--- Personal Projects/100 Days of Code/CoffeeMachine/CoffeeMachineActivate.py
+++ Personal Projects/100 Days of Code/CoffeeMachine/CoffeeMachineActivate.py
@@ -3,16 +3,41 @@ from CoffeeMachine import MachineItems
 from CoffeeMachine import MenuItems
 from CoffeeMachine import Money
 
-machine_water = MachineItems.machine_water
+machine_water = MachineItems().machine_water
 
-machine_milk = MachineItems.machine_milk
+machine_milk = MachineItems().machine_milk
 
-machine_coffee = MachineItems.machine_coffee
+machine_coffee = MachineItems().machine_coffee
 
-machine_money = MachineItems.machine_money
+machine_money = MachineItems().machine_money
 
-coffee_cost = MachineItems.coffee_cost
+coffee_cost = MachineItems().coffee_cost
 
-customer_money = MachineItems.customer_money
+customer_money = MachineItems().customer_money
 
-MachineFunctions.choose_coffee(MachineFunctions.process_cust_money(customer_money))
+machine_on = True 
+
+while machine_on == True:
+    MachineFunctions().balance(customer_money=customer_money)
+    choice = MachineFunctions().choose_coffee(choice_check=True)
+    if choice == "cancel":
+            machine_on = False
+            machine_money= MachineFunctions().return_change(machine_money=machine_money, customer_money=customer_money)
+            MachineFunctions().end_greeting(customer_money=customer_money)
+            customer_money = MachineItems().customer_money
+    elif choice == "report":
+        MachineFunctions().report(machine_water=machine_water, machine_milk=machine_milk, machine_coffee=machine_coffee, machine_money=machine_money)
+    else:
+        coffee_cost = MachineFunctions().get_coffee_cost(choice=choice)
+        money = MachineFunctions().process_cust_money(machine_money=machine_money, coffee_cost=coffee_cost, customer_money=customer_money, quarter=Money().quarter, dime=Money().dime, nickel=Money().nickel, penny=Money().penny, money_input_check=True)
+        customer_money = money[0]
+        machine_money = money[1]
+        cust_money_ok = MachineFunctions().check_money(choice=choice, customer_money=customer_money)
+        if cust_money_ok == True:
+            ingredients = MachineFunctions().make_coffee(choice, machine_water=machine_water, machine_coffee=machine_coffee, machine_milk= machine_milk)
+            if ingredients != False:
+                machine_water = ingredients[0]
+                machine_milk = ingredients[1]
+                machine_coffee = ingredients[2]
+                customer_money = MachineFunctions().take_cust_money(customer_money=customer_money, coffee_cost=coffee_cost)
+
